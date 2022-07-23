f3dbe3238aaf5a5958186e13a8e40489dc456404
Paulo Henrique <paulo.henrique.braguim@outlook.com>
2022-07-23 07:52:50 -0300

    Consumindo API Clima e Tempo - API Open Weather Map

---
+++ clima.php
@@ -0,0 +1,71 @@
+<?php
+
+//Autoload das classes
+require __DIR__ . "/vendor/autoload.php";
+
+//Dependências
+use App\WebService\Api;
+
+//Instância da Classe
+$api = new Api("5c113205eab4b011183a1695e7893864");
+
+$cidade = filter_input(INPUT_POST, "inputCidade");
+$uf = filter_input(INPUT_POST, "selectUf");
+
+if(strlen($cidade) < 1){   
+    header("Location: /"); exit;
+}else {
+    $dados = $api->consultarClima($cidade, $uf);
+    
+    if($dados['cod'] == "404"){
+        header("location: /public/views/erro.html"); exit;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado Pesquisa do Clima</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p><?php echo "Cidade: " . $dados['name'] ?></p><hr>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+            <p><?php echo "Temperatura: " . $dados['main']['temp'] ." °C" ?></p><hr>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+            <p><?php echo "Sensação Térmica: " . $dados['main']['feels_like'] ." °C" ?></p><hr>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <p><?php echo "Céu: " . $dados['weather'][0]['description']?></p><hr>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <p><?php echo "Velocidade do Vento: " . $dados['wind']['speed'] . " km"?></p><hr>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - Pesquisar Clima</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Clima e Tempo - API Open Weather Map</h3>
+            </div>
+        </div>
+        <br/>
+        <div class="row">
+            <form action="clima.php" method="POST" class="form" onsubmit="validacao()">
+                <div class="mb-3">
+                    <label for="inputCidade" class="form-label">Nome da Cidade</label>
+                    <input type="text" class="form-control" name="inputCidade" id="inputCidade" placeholder="Digite o nome da cidade">                    
+                </div>
+                <div class="mb-3">
+                <label for="selectUf" class="form-label">UF</label>
+                    <select class="form-select" id="selectUf" name="selectUf" aria-label="Default select example">
+                        <option selected>Selecione a UF</option>
+                        <option value="AC">AC</option>
+                        <option value="AL">AL</option>
+                        <option value="AM">AM</option>                        
+                        <option value="AP">AP</option>
+                        <option value="BA">BA</option>
+                        <option value="CE">CE</option>
+                        <option value="DF">DF</option>
+                        <option value="ES">ES</option>
+                        <option value="GO">GO</option>
+                        <option value="MA">MA</option>
+                        <option value="MG">MG</option>
+                        <option value="MS">MS</option>
+                        <option value="MT">MT</option>
+                        <option value="PA">PA</option>
+                        <option value="PB">PB</option>
+                        <option value="PE">PE</option>
+                        <option value="PI">PI</option>
+                        <option value="PR">PR</option>
+                        <option value="RJ">RJ</option>
+                        <option value="RN">RN</option>
+                        <option value="RO">RO</option>
+                        <option value="RR">RR</option>
+                        <option value="RS">RS</option>
+                        <option value="SC">SC</option>
+                        <option value="SE">SE</option>
+                        <option value="SP">SP</option>
+                        <option value="TO">TO</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary btn-style">Consultar</button>
+            </form>
+        </div>
+    </div>
+
+    <script>
+        function validacao(){
+            if(inputCidade.value < 3){
+                
+                alert("POR FAVOR, INFORME UMA CIDADE VÁLIDA!");
+                inputCidade.focus();
+                return false;
+                            
+            }
+        }
+    </script>
+
+</body>
+</html>
